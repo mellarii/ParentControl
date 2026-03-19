98a3f20de6ca66e7b0771c9d7d8f20accc6e7884
mellarii <art76394@gmail.com>
2026-03-19 17:51:27 +0300

    Readme UPD1

--- main.py
+++ main.py
@@ -3,38 +3,26 @@ import sys
 from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtWidgets import  QApplication, QWidget, QLabel, QPushButton
-
 from PyQt6.QtWidgets import  QApplication, QWidget, QLabel, QPushButton
 
 def main():
-
   app = QApplication(sys.argv)
   window = QWidget()
   state_window = QWidget(window, Qt.WindowType.Window)
   blacklist_window = QWidget(window, Qt.WindowType.Window)
 
   timer = QLabel("00:00", window)
-
   main_text = QLabel("JustBeggining", window)
-
   start_timer = QPushButton("Start", window)
-
   add_to_bunlist_btn = QPushButton("Add new sites to blacklist.", window)
-
   see_state = QPushButton("Device activity statistics", window)
 
   see_state.move(15, 465)
-
   add_to_bunlist_btn.move(15, 500)
-
   start_timer.move(440, 270)
-
   timer.move(283,270)
-
   timer.resize(400, 100)
-
   main_text.resize(400,100)
-
   main_text.move(444, 200)
 
   main_text = QLabel("JustBeggining", window)
@@ -55,13 +43,9 @@ def main():
   time_data = [0]
 
   def tick():
-
     time_data[0] += 1
-
     s = time_data[0]
-
     minutes = s //60
-
     seconds = s % 60
 
     timer.setText(f"{minutes:02}:{seconds:02}")
@@ -88,13 +72,10 @@ def main():
     else:
       clock.stop()
       startTimerBtn.setText("Resume")
-
   startTimerBtn.clicked.connect(lambda: startLogic())
 
   window.setWindowTitle("Parent Control")
-
   window.resize(960,540)
-
   window.resize(960,540)
   window.show()
 
@@ -108,24 +89,11 @@ def main():
   UsingTime = QLabel("Today you use: ", state_window)
   UsingTime.move(15, 55)
 
-
   blacklist_window.setWindowTitle("Blacklist")
   blacklist_window.resize(480, 270)
   banlistBtn.clicked.connect(lambda: blacklist_window.show())
 
-
-
   sys.exit(app.exec())
 
 if __name__ == "__main__":
-
-  # try: 
-
-  # try: 
     main()
-
-  # except Exception as e:
-
-  #   print("Error detected")
-  # except Exception as e:
-  #   print("Error detected")
